2fcfd76fc965c9388ea9388e258b2d672682e8e5
Onur Kapucu <kapucuonur@hotmail.comcat>
2025-02-05 23:27:46 +0200

    deploy prepping

--- blog/urls.py
+++ blog/urls.py
@@ -22,3 +22,5 @@ urlpatterns = [
     path('', include('posts.urls')),
     path('users/', include('users.urls')),
 ]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
